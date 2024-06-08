f13076a63b0498dff77e846bb503b7969a00f451
David Isaac <david.omokafe@fidelitybank.ng>
2024-06-08 20:55:22 +0100

    changes

--- CSharp01/Program.cs
+++ CSharp01/Program.cs
@@ -10,6 +10,14 @@ namespace Fundamentals
             var name = string.Format("My full name is {0} {1}", firstName, lastName);
 
             String[] names = new string[3] { "Miles", "David", "omo" };
+            Console.WriteLine(names);
+
+            var text = @"Hi Doe
+Look into the following path to see
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
+
         }
 
     }
