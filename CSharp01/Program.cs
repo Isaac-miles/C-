0fe230d3ba198f88041ad54b5640925610f90b48
David Isaac <david.omokafe@fidelitybank.ng>
2024-06-08 21:40:29 +0100

    changes

--- CSharp01/Program.cs
+++ CSharp01/Program.cs
@@ -1,10 +1,20 @@
 ﻿
 namespace Fundamentals
 {
+    public enum ShipMethods
+    {
+        SalesMail =1,
+        RegisteredUsers =2,
+        Express = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            var method = ShipMethods.SalesMail;
+            Console.WriteLine((Int32) method);
             string firstName = "Miles";
             string lastName = "Isaac";
             var name = string.Format("My full name is {0} {1}", firstName, lastName);
@@ -13,7 +23,7 @@ namespace Fundamentals
             Console.WriteLine(names);
 
             var text = @"Hi Doe
-Look into the following path to see
+Look into the following path to see the file
 c:\folder1\folder2
 c:\folder3\folder4";
             Console.WriteLine(text);
