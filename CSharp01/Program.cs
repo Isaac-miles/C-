48f2293d1f440ada01c0f2e1b7120afc989892b8
David Isaac <david.omokafe@fidelitybank.ng>
2024-06-08 20:48:23 +0100

    string methods

--- CSharp01/Program.cs
+++ CSharp01/Program.cs
@@ -5,7 +5,12 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
-            
+            string firstName = "Miles";
+            string lastName = "Isaac";
+            var name = string.Format("My full name is {0} {1}", firstName, lastName);
+
+            String[] names = new string[3] { "Miles", "David", "omo" };
         }
+
     }
 }
