18b97937a5da0865262b83265976ae7c71e22354
David Isaac <david.omokafe@fidelitybank.ng>
2024-06-08 22:29:54 +0100

    b

--- CSharp01/Program.cs
+++ CSharp01/Program.cs
@@ -1,34 +1,25 @@
 ﻿
 namespace Fundamentals
 {
-    public enum ShipMethods
-    {
-        SalesMail =1,
-        RegisteredUsers =2,
-        Express = 3
-    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Seasons seasons = Seasons.Rainy;
 
-            var method = ShipMethods.SalesMail;
-            Console.WriteLine((Int32) method);
-            string firstName = "Miles";
-            string lastName = "Isaac";
-            var name = string.Format("My full name is {0} {1}", firstName, lastName);
-
-            String[] names = new string[3] { "Miles", "David", "omo" };
-            Console.WriteLine(names);
-
-            var text = @"Hi Doe
-Look into the following path to see the file
-c:\folder1\folder2
-c:\folder3\folder4";
-            Console.WriteLine(text);
-
+            switch (seasons)
+            {
+                case Seasons.Rainy:
+                    Console.WriteLine("Its Raining Season already");
+                    break;
+                case Seasons.Hamattan:
+                    Console.WriteLine("Na Hamatan Jare");
+                    break;
+                default:
+                    Console.WriteLine("No Match found try again");
+                    break;
+            }
         }
-
     }
 }
