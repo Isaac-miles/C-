2b278f0be4c5abda4ae191a3858487f2f24b3e7b
David Isaac <david.omokafe@fidelitybank.ng>
2024-06-10 20:32:12 +0100

    arrays

---
+++ CSharp01/Arrays.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp01
+{
+     class Arrays
+    {
+        static void Main(string[] args)
+        {
+            var num = new[] { 3, 7, 9, 2, 14, 6 };
+            Console.WriteLine(num.Length);
+        }
+    }
+}
